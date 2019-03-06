605453098c2f05a201603c803453b5ba41cd63ea
vallecbg <valioplayofficial@gmail.com>
2019-03-06 16:38:50 +0200

    solution

---
+++ Databases Advanced - Entity Framework/Code First - Exercise/EF-Code-First/P01_HospitalDatabase.Data.Models/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Patient
+    {
+        public Patient()
+        {
+            this.Prescriptions = new HashSet<PatientMedicament>();
+            this.Visitations = new HashSet<Visitation>();
+            this.Diagnoses = new HashSet<Diagnose>();
+        }
+
+        public int PatientId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public bool HasInsurance { get; set; }
+
+        public ICollection<PatientMedicament> Prescriptions { get; set; }
+
+        public ICollection<Visitation> Visitations { get; set; }
+        public ICollection<Diagnose> Diagnoses { get; set; }
+    }
+}
---
+++ Databases Advanced - Entity Framework/Code First - Exercise/EF-Code-First/P01_HospitalDatabase/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using P01_HospitalDatabase.Data;
+using System;
+
+namespace P01_HospitalDatabase
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new HospitalContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
